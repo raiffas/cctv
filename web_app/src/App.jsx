82dc025d9c4bffdb95f9f6f0dc6ce7c964e0050c
raiffas <raiffas@gmail.com>
2026-07-29 16:18:19 -0400

    improving server down page

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import oscarImg from './assets/oscarTransparent.png'
+import oscarLaptopImg from './assets/images/oscarLaptop.jpg'
 import './App.css'
 
 import oscarVideo from './assets/videos/oscar2.mp4'
@@ -13,7 +14,7 @@ function LandingPage( {setShowLandingPage} ) {
               <div className="horizontal-stack">
                 <img src={oscarImg} className='oscar' ></img>
                 <div className="text-container" >
-                  <h1 className="double-text" data-text="CCTV">CCTV</h1>
+                  <h1 className="double-text" id="title" data-text="CCTV">CCTV</h1>
                 </div>
               </div>
 
@@ -30,7 +31,38 @@ function LandingPage( {setShowLandingPage} ) {
           </div>
 }
 
-function VideoPlayer( {setShowLandingPage} ) {
+function ServerDownPage( {setShowLandingPage} ) {
+  return <div className="center-vertical-stack">
+            <div className="text-container" >
+              <h1 className="double-text" id="page-header" data-text="uh oh">uh oh</h1>
+            </div>
+            <img src={oscarLaptopImg} id='laptop-image'/>
+            <div className="text-container" >
+              <h1 className="double-text" id="page-subheader" data-text="oscar turned off the server">oscar turned off the server</h1>
+            </div>
+            <div className='button-container'>
+              <button className="play" onClick={ () => { setShowLandingPage(true) } }>← RETURN HOME</button>
+            </div>
+
+        </div>
+}
+
+function VideoPlayer( {setShowLandingPage, loading, serverAuth} ) {
+  return <div className='container'>
+            <div className='left-vertical-stack'>
+              <div className='video-stream'>
+                  {loading ? (<img src={oscarImg} /> ) : null}
+                  {serverAuth ? ( <img src='https://laptop.elver-mimosa.ts.net:5000/video-feed' onLoad={() => setLoading(false)}/> ) : <video src={oscarVideo} autoPlay loop/>}
+              </div>
+              <div className='button-container'>
+                <button className="play" onClick={ () => { setShowLandingPage(true) } }>← RETURN HOME</button>
+              </div>
+            </div>
+          </div>
+
+}
+
+function VideoPage( {setShowLandingPage} ) {
     const [serverAuth, setServerAuth] = useState(false)
     const [loading, setLoading] = useState()
     
@@ -52,17 +84,10 @@ function VideoPlayer( {setShowLandingPage} ) {
     })();
   }, [setServerAuth])
 
-  return  <div className='container'>
-            <div className='left-vertical-stack'>
-              <div className='video-stream'>
-                  {loading ? (<img src={oscarImg} /> ) : null}
-                  {serverAuth ? ( <img src='https://laptop.elver-mimosa.ts.net:5000/video-feed' onLoad={() => setLoading(false)}/> ) : <video src={oscarVideo} autoPlay loop/> }
-              </div>
-              <div className='button-container'>
-                <button className="play" onClick={ () => { setShowLandingPage(true) } }>← RETURN HOME</button>
-              </div>
-            </div>
-          </div>
+  return ( 
+    <>
+    { serverAuth ? <VideoPlayer setShowLandingPage={setShowLandingPage} /> : <ServerDownPage setShowLandingPage={setShowLandingPage} /> }
+    </> )
 }
 
 function App() {
@@ -70,7 +95,7 @@ function App() {
 
   return (
     <>
-      { showLandingPage ?  <LandingPage setShowLandingPage={setShowLandingPage} />  : <VideoPlayer setShowLandingPage={setShowLandingPage}  />  }
+      { showLandingPage ?  <LandingPage setShowLandingPage={setShowLandingPage} />  : <VideoPage setShowLandingPage={setShowLandingPage}  />  }
     </>
   )
 }
